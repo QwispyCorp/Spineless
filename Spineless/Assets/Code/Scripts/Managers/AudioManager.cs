643c1a4abab90c79e84f34739c55a7253b9ad4f7
jodortiz <jodortiz@ucsc.edu>
2024-01-12 19:56:15 -0800

    Audio manager optimizations and updates

--- Spineless/Assets/Code/Scripts/Managers/AudioManager.cs
+++ Spineless/Assets/Code/Scripts/Managers/AudioManager.cs
@@ -1,24 +1,29 @@
 using UnityEngine.Audio;
 using UnityEngine;
 using System;
-using Unity.VisualScripting;
 
-//This is an Audio Manager done by Brakeys on YouTube
+using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
 
 public class AudioManager : MonoBehaviour
 {
-    public Sound[] sounds;
+    //list of sounds
+    [SerializeField] private Sound[] sounds;
 
-    public static AudioManager instance;
+    //local and global manager instances
+    private static AudioManager _instance;
+    public static AudioManager Instance { get { return _instance; } } //to use any public method call AudioManager.Instance."FunctionName"(); anywhere in any script
 
     void Awake()
     {
-        if (instance == null)
-            instance = this;
+        //on awake check for existence of manager and handle accordingly
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
         else
         {
-            Destroy(gameObject);
-            return;
+            _instance = this;
         }
 
         DontDestroyOnLoad(gameObject);
@@ -31,14 +36,23 @@ public class AudioManager : MonoBehaviour
             s.source.volume = s.volume;
             s.source.pitch = s.pitch;
             s.source.loop = s.loop;
+            s.source.playOnAwake = s.playOnAwake;
         }
     }
 
     private void Start() // this will play sounds on start of the level, so main theme and what not
     {
-        //Play("AudioName");
+        if (SceneManager.GetActiveScene().name == "Main Menu")
+        {
+            //AudioManager.Instance.Play("Title Music");  uncomment this when title music is added
+        }
     }
-    public void Play(string name)
+
+    /* -------------------------------------------------------------------------- */
+    /*                       AudioManager Methods                                 */
+    /* -------------------------------------------------------------------------- */
+    
+    private void Play(string name)  //Play a sound using its name as input. Usage: AudioManager.Instance.Play("name");
     {
         Sound s = Array.Find(sounds, sound => sound.name == name);
         if (s == null)// if name of audio clip is wrong this will help debug
@@ -48,7 +62,14 @@ public class AudioManager : MonoBehaviour
         }
         s.source.Play();
     }
-
+    private void Stop(string name) //Stop a sound using its name as input. Usage: AudioManager.Instance.Stop("name");
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)// if name of audio clip is wrong this will help debug
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+        s.source.Stop();
+    }
 }
-// Below is the code you put to reference the audio clip anywhere in the script. 
-// FindObjectOfType<AudioManager>().Play("AudioName");
