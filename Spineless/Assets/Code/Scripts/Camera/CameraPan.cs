8b7a84aaea80d2b52c0405b074c518f8e34c4c20
eqwisp <eacrisp@ucsc.edu>
2024-02-13 22:45:06 -0800

    Camera Update

--- Spineless/Assets/Code/Scripts/Camera/CameraPan.cs
+++ Spineless/Assets/Code/Scripts/Camera/CameraPan.cs
@@ -1,51 +1,29 @@
-using System.Collections;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 public class CameraPan : MonoBehaviour
 {
     public float senseX;
     public float senseY;
-    public bool CanLook;
     public Transform orientation;
-    private Quaternion initialRotation;
-    private Vector3 initialPosition;
-    private float initialYRotation;
     float xRotation;
     float yRotation;
+    public bool CanLook;
+    private Quaternion initialRotation;
+    private Vector3 initialPosition;
+
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = true;
+        CanLook = true;
         initialRotation = transform.localRotation;
         initialPosition = transform.localPosition;
-        initialYRotation = orientation.localRotation.eulerAngles.y;
     }
-    //--------------------------------------------------------
-    public void ResetCamera()
-    {
-        StartCoroutine(ResetCameraCoroutine(0.5f));
-    }
-
-    IEnumerator ResetCameraCoroutine(float duration)
-    {
-        float elapsedTime = 0f;
-        Quaternion startRotation = transform.localRotation;
-        Vector3 startPosition = transform.localPosition;
-
-        while (elapsedTime < duration)
-        {
-            elapsedTime += Time.deltaTime;
-            float t = Mathf.Clamp01(elapsedTime / duration);
-            transform.localRotation = Quaternion.Slerp(startRotation, initialRotation, t);
-            transform.localPosition = Vector3.Lerp(startPosition, initialPosition, t);
-            yield return null;
-        }
-        transform.localRotation = initialRotation;
-        transform.localPosition = initialPosition;
-    }
-
     //--------------------------------------------------------
     void Update()
-    {   
+    {
+        if (CanLook == true)
+        {
             float mouseX = -Input.GetAxisRaw("Mouse X") * Time.deltaTime * senseX;
             float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * senseY;
 
@@ -55,5 +33,12 @@ public class CameraPan : MonoBehaviour
 
             transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0);
             orientation.localRotation = Quaternion.Euler(0, yRotation, 0);
+        }
+    }
+    //--------------------------------------------------------
+    public void ResetCameraPosition()
+    {
+        transform.localRotation = initialRotation;
+        transform.localPosition = initialPosition;
     }
 }
--- Spineless/Assets/Code/Scripts/Camera/CinemaCam.cs
+++ Spineless/Assets/Code/Scripts/Camera/CinemaCam.cs
@@ -1,32 +1,111 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CinemaCam : MonoBehaviour
 {
+    public GameObject Cam1;
+    public GameObject Cam2;
+    public Animator CamAni;
+    public CameraPan CamPanScript;
+    private Quaternion initialRotation;
+    private Vector3 initialPosition;
     //--------------------------------------------------------
-    public void CameraUnlock()
+    private void Start()
     {
-        StartCoroutine(CamUnlockCoroutine());
+        initialRotation = Cam1.transform.localRotation; 
+        initialPosition = Cam1.transform.localPosition;
+    }
+    //--------------------------------------------------------
+    public void ResetCam()
+    {
+        StartCoroutine(ResetCameraCoroutine(Cam1.transform, 0.5f));
     }
 
-    IEnumerator CamUnlockCoroutine()
+    IEnumerator ResetCameraCoroutine(Transform cameraTransform, float duration)
     {
-        yield return new WaitForSeconds(2f);
+        float elapsedTime = 0f;
+        Quaternion startRotation = cameraTransform.localRotation;
+        Vector3 startPosition = cameraTransform.localPosition;
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            float t = Mathf.Clamp01(elapsedTime / duration);
+            cameraTransform.localRotation = Quaternion.Slerp(startRotation, initialRotation, t);
+            cameraTransform.localPosition = Vector3.Lerp(startPosition, initialPosition, t);
+            yield return null;
+        }
+    }
+
+    //--------------------------------------------------------
+    public void Free2Cabinet() //CALL THIS TO START THIS ANIMATION
+    {
+        StartCoroutine(Free2CabinetCoroutine());
+    }
+    IEnumerator Free2CabinetCoroutine()
+    {
+        ResetCam();
+        CamPanScript.CanLook = false;
+        yield return new WaitForSeconds(0.6f);
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.None;
+        CamPanScript.ResetCameraPosition();
+        Cam1.SetActive(false);
+        Cam2.SetActive(true);
+        CamAni.SetTrigger("F2C");
+        yield return new WaitForSeconds(1.1f);
         Cursor.lockState = CursorLockMode.Confined;
         Cursor.visible = true;
     }
     //--------------------------------------------------------
-    public void Cameralock()
+    public void Cabinet2Free() //CALL THIS TO START THIS ANIMATION
     {
-        StartCoroutine(CamLockCoroutine());
+        StartCoroutine(Cabinet2FreeCoroutine());
     }
-
-    IEnumerator CamLockCoroutine()
+    IEnumerator Cabinet2FreeCoroutine()
     {
-        yield return new WaitForSeconds(2f);
+        Cursor.visible = false;
+        CamAni.SetTrigger("C2F");
+        yield return new WaitForSeconds(1f);
+        Cam1.SetActive(true);
+        //CamPanScript.ResetCameraPosition();
+        Cam2.SetActive(false);
+        yield return new WaitForSeconds(0.5f);
+        CamPanScript.CanLook = true;
+    }
+    //--------------------------------------------------------
+    public void Free2board() //CALL THIS TO START THIS ANIMATION
+    {
+        StartCoroutine(Free2BoardCoroutine());
+    }
+    IEnumerator Free2BoardCoroutine()
+    {
+        ResetCam();
+        CamPanScript.CanLook = false;
+        yield return new WaitForSeconds(0.6f);
+        //CamPanScript.ResetCameraPosition();
+        Cam1.SetActive(false);
+        Cam2.SetActive(true);
+        CamAni.SetTrigger("F2B");
+        yield return new WaitForSeconds(1.1f);
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+    }
+    //--------------------------------------------------------
+    public void Board2Free() //CALL THIS TO START THIS ANIMATION
+    {
+        StartCoroutine(Board2FreeCoroutine());
+    }
+    IEnumerator Board2FreeCoroutine()
+    {
+        Cursor.visible = false;
+        CamAni.SetTrigger("B2F");
+        yield return new WaitForSeconds(1f);
+        Cam1.SetActive(true);
+        Cam2.SetActive(false);
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        yield return new WaitForSeconds(0.5f);
+        CamPanScript.CanLook = true;
     }
-
 }
