3634ff04d0d17f874d6b9dfc1239379a8d2bc635
eqwisp <134550496+Elliotissocool@users.noreply.github.com>
2024-03-08 18:30:39 -0800

    pushhhh

--- Spineless/Assets/ItemTextCloser.cs
+++ Spineless/Assets/ItemTextCloser.cs
@@ -27,6 +27,35 @@ public class ItemTextCloser : MonoBehaviour
                     }
                     else
                     {
+                        Debug.LogWarning("Could not find text object for " + item.itemName + ".");
+                    }
+                }
+                else if (!saveData.EquippedItems.Find(x => x.name == item.itemName)) //if the item is not in inventoiry AND is not in equipped items, turn off its screen in this room
+                {
+                    //Find the item's corresponding text object in the scene
+                    if (GameObject.Find(item.itemName + " Text") != null)
+                    {
+                        Debug.Log(item.itemName + " Text Object Found");
+                        GameObject itemTextObject = GameObject.Find(item.itemName + " Text");
+                        itemTextObject.SetActive(false);
+                    }
+                    else
+                    {
+                        item.itemName = null;
+                        Debug.LogWarning("Could not find text object for " + item.itemName + ".");
+                    }
+                }
+                else if (!saveData.Inventory.Find(x => x.name == item.itemName)) //if the item is not in inventoiry AND is not in equipped items, turn off its screen in this room
+                {
+                    //Find the item's corresponding text object in the scene
+                    if (GameObject.Find(item.itemName + " Text") != null)
+                    {
+                        Debug.Log(item.itemName + " Text Object Found");
+                        GameObject itemTextObject = GameObject.Find(item.itemName + " Text");
+                        itemTextObject.SetActive(false);
+                    }
+                    else
+                    {
                         item.itemName = null;
                         Debug.LogWarning("Could not find text object for " + item.itemName + ".");
                     }
