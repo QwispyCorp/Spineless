71f7204834be35bc043e7ca3ecd640f6fff0e5c6
jodortiz <jodortiz@ucsc.edu>
2024-03-10 22:34:37 -0700

    da wings are innnn

--- Spineless/Assets/Code/Scripts/Managers/WingManager.cs
+++ Spineless/Assets/Code/Scripts/Managers/WingManager.cs
@@ -3,9 +3,7 @@ using UnityEngine;
 
 public class WingManager : MonoBehaviour
 {
-    public List<GameObject> wing1Prefabs;
-    public List<GameObject> wing2Prefabs;
-    public List<GameObject> wing3Prefabs;
+    public List<GameObject> wingPrefabs;
 
     public Transform position1;
     public Transform position2;
@@ -13,11 +11,32 @@ public class WingManager : MonoBehaviour
 
     void Start()
     {
-        GameObject wing1Prefab = wing1Prefabs[Random.Range(0, wing1Prefabs.Count)];
-        GameObject wing2Prefab = wing2Prefabs[Random.Range(0, wing2Prefabs.Count)];
-        GameObject wing3Prefab = wing3Prefabs[Random.Range(0, wing3Prefabs.Count)];
-        Instantiate(wing1Prefab, position1.position, position1.rotation);
-        Instantiate(wing2Prefab, position2.position, position2.rotation);
-        Instantiate(wing3Prefab, position3.position, position3.rotation);
+        //WING 1: find and instantiate a random wing for position 1
+        int wing1PrefabIndex = Random.Range(0, wingPrefabs.Count);
+        GameObject wing1Prefab = wingPrefabs[wing1PrefabIndex];
+        GameObject _wing1 = Instantiate(wing1Prefab, position1.position, position1.rotation);
+        _wing1.transform.parent = position1;
+
+        //WING 2: find and instantiate a random wing not spawned in first position
+        int wing2PrefabIndex;
+        do
+        {
+            wing2PrefabIndex = Random.Range(0, wingPrefabs.Count);
+        } while (wing2PrefabIndex == wing1PrefabIndex);
+
+        GameObject wing2Prefab = wingPrefabs[wing2PrefabIndex];
+        GameObject _wing2 = Instantiate(wing2Prefab, position2.position, position2.rotation);
+        _wing2.transform.parent = position2;
+
+        //WING 3: find and instantiate a random wing not spawned in first or second position
+        int wing3PrefabIndex;
+        do
+        {
+            wing3PrefabIndex = Random.Range(0, wingPrefabs.Count);
+        } while (wing3PrefabIndex == wing1PrefabIndex || wing3PrefabIndex == wing2PrefabIndex);
+
+        GameObject wing3Prefab = wingPrefabs[wing3PrefabIndex];
+        GameObject _wing3 = Instantiate(wing3Prefab, position3.position, position3.rotation);
+        _wing3.transform.parent = position3;
     }
 }
